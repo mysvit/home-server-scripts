684e98cb72674c54cd18032f62f56c6e9e06d49d
sv0505 <sv0505@gmail.com>
2020-03-01 21:39:30 -0500

    Update 1-cloudcmd.sh

--- 1-cloudcmd.sh
+++ 1-cloudcmd.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-apt update
-apt -y install curl dirmngr apt-transport-https lsb-release ca-certificates
+apt install curl dirmngr apt-transport-https lsb-release ca-certificates -y
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
-apt install nodejs
-npm i cloudcmd -g
+apt install nodejs -y
+npm install cloudcmd -g
