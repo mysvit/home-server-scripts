e63317367fe07924fb6e4eb1929b65893a750ad6
sv0505 <sv0505@gmail.com>
2020-03-02 22:55:10 -0500

    Update 1-cloudcmd.sh

--- 1-cloudcmd.sh
+++ 1-cloudcmd.sh
@@ -3,3 +3,20 @@ apt install curl dirmngr apt-transport-https lsb-release ca-certificates -y
 curl -sL https://deb.nodesource.com/setup_12.x | bash -
 apt install nodejs -y
 npm install cloudcmd -g
+
+echo -n "enter username : "
+read my_user
+echo -n "enter password : "
+read my_pwd
+cloudcmd --username $my_user --password $my_pwd --auth --save --no-server
+
+srv = "/etc/systemd/system/cloudcmd.service"
+echo "[Unit]" >> $srv
+echo "Description=Cloud Commander" >> $srv
+echo "After=network-online.target" >> $srv
+echo "[Service]" >> $srv
+echo "Type=simple" >> $srv
+echo "ExecStart=/usr/bin/cloudcmd" >> $srv
+echo "Restart=on-failure" >> $srv
+echo "[Install]" >> $srv
+echo "WantedBy=multi-user.target" >> $srv
