13f0881cc2cec52a268b24c91e0e2dd26fef7cef
sv0505 <sv0505@gmail.com>
2020-03-04 00:03:01 -0500

    Create 2-deluge.sh

---
+++ 2-deluge.sh
@@ -0,0 +1,41 @@
+#!/usr/bin/env bash
+#LOCATION your-server-ip:8112
+
+apt install deluged deluge-webui -y
+adduser --no-create-home --system --group deluge
+
+srv="/etc/systemd/system/deluged.service"
+echo "[Unit]">>$srv
+echo "Description=Deluge Bittorrent Client Daemon">>$srv
+echo "After=network-online.target">>$srv
+echo "[Service]">>$srv
+echo "Type=simple">>$srv
+echo "User=deluge">>$srv
+echo "Group=deluge">>$srv
+echo "UMask=022">>$srv
+echo "ExecStart=/usr/bin/deluged -d">>$srv
+echo "Restart=on-failure">>$srv
+echo "TimeoutStopSec=300">>$srv
+echo "[Install]">>$srv
+echo "WantedBy=multi-user.target">>$srv
+
+systemctl enable deluged
+systemctl start deluged
+
+
+srv="/etc/systemd/system/deluge-web.service"
+echo "[Unit]">>$srv
+echo "Description=Deluge Bittorrent Client Web Interface">>$srv
+echo "After=network-online.target">>$srv
+echo "[Service]">>$srv
+echo "Type=simple">>$srv
+echo "User=deluge">>$srv
+echo "Group=deluge">>$srv
+echo "UMask=027">>$srv
+echo "ExecStart=/usr/bin/deluge-web">>$srv
+echo "Restart=on-failure">>$srv
+echo "[Install]">>$srv
+echo "WantedBy=multi-user.target">>$srv
+
+systemctl enable deluge-web
+systemctl start deluge-web
