21f29e5526bb346e414d841cc42bdf3197151235
sv0505 <sv0505@gmail.com>
2020-03-05 14:26:31 -0500

    Update samba.sh

--- samba.sh
+++ samba.sh
@@ -10,8 +10,13 @@
 apt update
 apt install samba -y
 
+#workgroup = MEDIA-WORLD
 #[work]
 #comment = work
 #read only = no
 #path = /home
 #guest ok = no
+
+# add user 
+# smbpasswd -a [username]
+systemctl restart smbd
