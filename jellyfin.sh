ccebe28adb57dab4c5cda9eddf3869e8848ae8f0
sv0505 <sv0505@gmail.com>
2020-03-05 18:50:48 -0500

    Create jellyfin.sh

---
+++ jellyfin.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+apt install apt-transport-https
+wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | apt-key add -
+echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | tee /etc/apt/sources.list.d/jellyfin.list
+apt update
+apt install jellyfin -y
