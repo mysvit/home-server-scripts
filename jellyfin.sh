5a19ffb0c20179b788121d1d9ccef9b64de8483a
sv0505 <sv0505@gmail.com>
2020-03-15 20:54:59 -0400

    Update jellyfin.sh

--- jellyfin.sh
+++ jellyfin.sh
@@ -1,8 +1,18 @@
 #!/usr/bin/env bash
 
+# Install jellyfin
 apt update
 apt install -y wget apt-transport-https ca-certificates curl gnupg2 software-properties-common
 wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | apt-key add -
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | tee /etc/apt/sources.list.d/jellyfin.list
 apt update
 apt install jellyfin -y
+
+#restore from backup
+systemctl stop jellyfin
+unzip -o /mnt/backup/jellyfin/var-lib-jellyfin.zip -d /
+
+chown -R jellyfin:jellyfin /var/lib/jellyfin/*
+find /var/lib/jellyfin -type d -exec chmod 755 {} \;
+find /var/lib/jellyfin -type f -exec chmod 644 {} \;
+ 
