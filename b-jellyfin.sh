7ae07f481591af5148238aea4fd9a598bbeb9d25
sv0505 <sv0505@gmail.com>
2020-03-15 21:47:21 -0400

    Update b-jellyfin.sh

--- b-jellyfin.sh
+++ b-jellyfin.sh
@@ -2,6 +2,8 @@
 
 # backup
 # data folder
+rm /mnt/backup/jellyfin/var-lib-jellyfin.zip
 zip -r1 /mnt/backup/jellyfin/var-lib-jellyfin.zip /var/lib/jellyfin/*
 # config
+rm /mnt/backup/jellyfin/etc-jellyfin.zip
 zip -r1 /mnt/backup/jellyfin/etc-jellyfin.zip /etc/jellyfin/*
