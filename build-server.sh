a3a54eee4e0eb7059acd1c493e0d0b27cacc4939
sv0505 <sv0505@gmail.com>
2020-03-21 19:17:32 -0400

    Update build-server.sh

--- build-server.sh
+++ build-server.sh
@@ -1,12 +1,55 @@
 #!/usr/bin/env bash
 
-# 1. Network setup
+echo
+echo
+echo
+echo
+echo
 
-
-
-# 1. Mount folders to mnt
-# 1.1. backup 
-# 1.2. media
-# 1.3. torrent work folders
+echo 0 Exit
+echo
+echo Network setup
+echo 11 interfaces
+echo 12 interfaces
+echo
+echo 2 Mount folders to mnt
+echo 21 backup 
+echo 22 media
+echo
+echo 31 torrent work folders
 
 # 2. 
+
+echo -n "Enter number:"
+read SELECT
+
+case $SELECT in
+
+  0)
+    echo -n "0"
+    ;;
+
+  11)
+    echo -n "11"
+    bash build-server.sh
+    ;;
+
+  12)
+    echo -n "12"
+    bash build-server.sh
+    ;;
+
+  21)
+    echo -n "12"
+    bash build-server.sh
+    ;;
+
+  22)
+    echo -n "12"
+    bash build-server.sh
+    ;;
+
+  *)
+    echo -n "unknown"
+    ;;
+esac
