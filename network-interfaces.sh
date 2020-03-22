33ef18dfdecc3de661a2080e0ac652d85dd39db1
sv0505 <sv0505@gmail.com>
2020-03-22 18:46:27 -0400

    Update network-interfaces.sh

--- network-interfaces.sh
+++ network-interfaces.sh
@@ -4,7 +4,7 @@ echo "List all interfaces"
 ip link show
 
 echo
-echo -n "Enter interface what you want add like static or press ENTER to exit : "
+echo -n "Enter interface what you want add like static or press ENTER to see interfaces : "
 read intStat
 
 if [ ! -z "$intStat" ]
@@ -19,9 +19,13 @@ echo "#netmask 255.255.255.0"         >>$f
 echo "#gateway 192.168.1.1"           >>$f
 echo "#dns-nameservers 192.168.1.1"   >>$f
 
-nano $f
+  nano $f
 
-bash network-restart.sh
+  bash network-restart.sh
+
+else
+
+  nano $f
 
 fi
 
