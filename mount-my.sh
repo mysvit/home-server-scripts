d8f48c5fecbc56d5b5d5e018608b57f68d124ff5
sv0505 <sv0505@gmail.com>
2020-03-29 19:55:56 -0400

    Update mount-my.sh

--- mount-my.sh
+++ mount-my.sh
@@ -1,23 +1,26 @@
 #!/bin/bash
 # mount usb on startup
 
-f="/etc/fstab"
-mounMedia = "mount /dev/sdc /mnt/usb"
+f-fstab="/etc/fstab"
+usb-hdd-8T="/dev/sdc"
+mnt-media="/mnt/media"
+
+mounMedia = "mount $usb-hdd-8T $mnt-media"
 
 echo "List usb devices : "
 fdisk -l
 
 echo
-echo ###################################################################
-echo $myInterface
-echo ###################################################################
+echo "###################################################################"
+echo "$mounMedia"
+echo "###################################################################"
 echo
 echo -n "Do you want to add you custome mount Y/n : "
 read yesNo
 
-if [ "$yesNo" = "Y" || "$yesNo" = "y"  ]
+if [ "$yesNo" = "Y" ] || ["$yesNo" = "y" ]
 then
-  echo $mounMedia > $f
+  echo $mounMedia >> $f-fstab
 fi
 
 nano $f
