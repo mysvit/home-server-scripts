9c3fa194843d8f37a702bd0bf5a479c73028aad8
sv0505 <sv0505@gmail.com>
2020-03-29 17:02:13 -0400

    Update network-my.sh

--- network-my.sh
+++ network-my.sh
@@ -15,6 +15,7 @@ netmask 255.255.255.0
 gateway 192.168.1.1
 dns-nameservers 192.168.1.1"
 
+
 echo "List all interfaces : "
 ip link show
 
@@ -26,7 +27,7 @@ echo
 echo -n "Do you want to apply you custome interfaces Y/n : "
 read yesNo
 
-if [ "$yesNo" = "Y" || "$yesNo" = "y"  ]
+if [ "$yesNo" == "Y" || "$yesNo" == "y"  ]
 then
   echo $myInterface > $f
   nano $f
