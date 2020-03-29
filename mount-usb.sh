c5de96540d02b4c50106592840275e426f239a80
sv0505 <sv0505@gmail.com>
2020-03-29 14:17:24 -0400

    Update mount-usb.sh

--- mount-usb.sh
+++ mount-usb.sh
@@ -3,17 +3,10 @@
 f="/etc/fstab"
 fdisk -l
 
+mountLine = "mount /dev/sdc /mnt/usb"
+
 echo "Enter [/dev/sdc?] what you want to mount to [/mnt/usb?] : "
-echo -n "[/dev/sdc?] : "
-read usbMedia
-echo 
-echo -n "[/mnt/usb?]"
-read usbFolder
+read $mountLine
 
-echo "mount $usbMedia $usbFolder" >> $f
+echo $mountLine >> $f
 nano $f
-
-# startup mount samba share
-#echo "//192.168.1.1/MEDIA/media /mnt/media cifs username=[username],password=[password],vers=1.0,uid=nobody,iocharset=utf8,noperm 0 0
-#">>$srv
-
