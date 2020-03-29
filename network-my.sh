499aa0aade0bd660c3726759baf5eb9d62cf8952
sv0505 <sv0505@gmail.com>
2020-03-29 19:15:22 -0400

    Update network-my.sh

--- network-my.sh
+++ network-my.sh
@@ -15,7 +15,7 @@ iface $intStat inet static
 address 192.168.1.10
 netmask 255.255.255.0
 gateway 192.168.1.1
-dns-nameservers 192.168.1.1"
+dns-nameservers 192.168.1.1 8.8.8.8"
 
 
 echo "###################################################################"
