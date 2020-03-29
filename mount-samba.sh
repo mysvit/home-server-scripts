1c49e2527a71a5b9de9fc5657edbca53de6f4e2f
sv0505 <sv0505@gmail.com>
2020-03-29 14:02:37 -0400

    Create mount-samba.sh

---
+++ mount-samba.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# mount Samba on startup
+f="/etc/fstab"
+fdisk -l
+
+echo "Startup mount samba share : "
+read
+echo "//192.168.1.1/remoteSambaFolder /mnt/mountFolder cifs username=[username],password=[password],vers=1.0,uid=nobody,iocharset=utf8,noperm 0 0" >> $f
+
+nano $f
