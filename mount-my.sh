9a43c882fee17b9be1a38970af4985d73f41318e
sv0505 <sv0505@gmail.com>
2020-03-29 20:59:01 -0400

    Update mount-my.sh

--- mount-my.sh
+++ mount-my.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
-# mount usb on startup
 
+# media
 f-fstab="/etc/fstab"
-usb-hdd-8T="/dev/sdc"
+usb-hdd-8T="/dev/sdc1"
 mnt-media="/mnt/media"
-
 mounMedia = "mount $usb-hdd-8T $mnt-media"
+
+# torrent deluge
+usb-hdd-500G="/dev/sdc2"
+mnt-deluge="/mnt/deluge"
+mounDeluge = "mount $usb-hdd-500G $mnt-deluge"
 
 echo "List usb devices : "
 fdisk -l
@@ -13,14 +17,16 @@ fdisk -l
 echo
 echo "###################################################################"
 echo "$mounMedia"
+echo "$mounDeluge"
 echo "###################################################################"
 echo
-echo -n "Do you want to add you custome mount Y/n : "
+echo -n "Do you want to add this custome mount to $f-fstab Y/n : "
 read yesNo
 
 if [ "$yesNo" = "Y" ] || ["$yesNo" = "y" ]
 then
   echo $mounMedia >> $f-fstab
+  echo $mounDeluge >> $f-fstab
 fi
 
-nano $f
+nano $f-fstab
