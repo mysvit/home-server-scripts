4493e25bdc679e6317068ddd1c87dc40a57bee88
sv0505 <sv0505@gmail.com>
2020-03-29 21:48:31 -0400

    Update mount-my.sh

--- mount-my.sh
+++ mount-my.sh
@@ -3,12 +3,12 @@
 # media
 f-fstab="/etc/fstab"
 usb-hdd-8T="/dev/sdc1"
-mnt-media="/mnt/media"
+mnt-media="/mnt/hdd-8T-1"
 mounMedia = "mount $usb-hdd-8T $mnt-media"
 
 # torrent deluge
 usb-hdd-500G="/dev/sdc2"
-mnt-deluge="/mnt/deluge"
+mnt-deluge="/mnt/hdd-500G-1"
 mounDeluge = "mount $usb-hdd-500G $mnt-deluge"
 
 echo "List usb devices : "
