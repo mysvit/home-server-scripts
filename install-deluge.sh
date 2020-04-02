7528b5b0c2ad79d737acd8df00177b9c9d6f7e83
sv0505 <sv0505@gmail.com>
2020-04-01 21:52:47 -0400

    Update and rename deluge.sh to install-deluge.sh

---
+++ install-deluge.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# config located /home/deluge/.config/core.conf
+#open deluge web your-server-ip:8112
+# default password = deluge
+# change password, download folder and compleader folder
+
+apt install deluged deluge-webui -y
+adduser --system --group deluge
+
+f-deluged="/etc/systemd/system/deluged.service"
+echo "[Unit]
+Description=Deluge Bittorrent Client Daemon
+After=network-online.target
+[Service]
+Type=simple
+User=deluge
+Group=deluge
+UMask=022
+ExecStart=/usr/bin/deluged -d
+Restart=on-failure
+TimeoutStopSec=300
+[Install]
+WantedBy=multi-user.target" > $f-deluged
+
+systemctl enable deluged
+systemctl start deluged
+
+
+f-deluge-web="/etc/systemd/system/deluge-web.service"
+echo "[Unit]
+Description=Deluge Bittorrent Client Web Interface
+After=network-online.target
+[Service]
+Type=simple
+User=deluge
+Group=deluge
+UMask=027
+ExecStart=/usr/bin/deluge-web
+Restart=on-failure
+[Install]
+WantedBy=multi-user.target" > $f-deluge-web
+
+systemctl enable deluge-web
+systemctl start deluge-web
