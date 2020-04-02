925dffab8af1bda051b4e52abdc649d637976ea7
sv0505 <sv0505@gmail.com>
2020-04-02 18:26:05 -0400

    Update mount-my.sh

--- mount-my.sh
+++ mount-my.sh
@@ -12,12 +12,13 @@ mnt-hdd-500G-1="/mnt/hdd-500G-1"
 mount-500G-1 = "mount $usb-hdd-500G-1 $mnt-hdd-500G-1"
 
 # media, torrent(deluge), backup folders
-d-media="$mnt-hdd-8T-1/media"
-sl-media="/mnt/media"
 d-deluge=$mnt-hdd-500G-1
 sl-deluge="/mnt/deluge"
+d-media="$mnt-hdd-8T-1/media"
+sl-media="/mnt/media"
 d-backup="$mnt-hdd-8T-1/backup"
 sl-backup="/mnt/backup"
+d-backup-jellyfin="$mnt-hdd-8T-1/backup/jellyfin"
 
 echo "List usb devices : "
 fdisk -l
@@ -39,6 +40,7 @@ then
   echo $mount-500G-1 >> $f-fstab
   nano $f-fstab
   mount -a
+  mkdir $d-backup-jellyfin
   ln -s $d-media $sl-media
   ln -s $d-deluge $sl-deluge
   ln -s $d-backup $sl-backup
