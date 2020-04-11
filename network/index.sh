3ef5ede2759feb9aca2b02abe2bbfa9011c9e345
sv0505 <sv0505@gmail.com>
2020-04-11 14:49:57 -0400

    Create index.sh

---
+++ network/index.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+clear 
+echo "
+0 Go back
+1 Restart network
+2 Interfaces
+3 Hostname
+"
+
+echo -n "Enter number:"
+read SELECT
+
+if [ $SELECT -eq 0 ]
+then
+    cd ..
+else
+
+case $SELECT in
+
+  1)
+    bash network-restart.sh
+    ;;
+  2)
+    bash network-interfaces.sh
+    ;;
+  3)
+    bash network-hostname.sh
+    ;;
+
+  *)
+    echo "unknown option pres ENTER to restart : "
+    read
+    ;;
+esac
+
+# run script again simulate goto to begin
+bash index.sh
+
+fi
