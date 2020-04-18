89e1d0182a5f1931c461d7118798d0430f3d7733
mysvit <sv0505@gmail.com>
2020-04-18 19:17:31 -0400

    Update disk-mount-usb.sh

--- disk/disk-mount-usb.sh
+++ disk/disk-mount-usb.sh
@@ -6,14 +6,20 @@ fdisk -l
 echo
 echo "##########  mount usb and put on startup ##########"
 echo
-echo -n "Enter device what you want to mount [/dev/sdc for example] : "
+echo -n "Enter device what you want to mount [/dev/sdb1 for example] : "
 read MOUNT_DEVICE
 [ -z $MOUNT_DEVICE ] && echo "Please provide mount device." && exit
 
 echo -n "Enter mount path [/mnt/usb for example] : "
 read MOUNT_PATH
 [ -z $MOUNT_PATH ] && echo "Please provide mount path." && exit
-[ ! -d $MOUNT_PATH ] && echo "Directory $MOUNT_PATH DOES NOT exists." && exit
+if [ !-d $MOUNT_PATH ]
+then
+  echo -n "Directory $MOUNT_PATH DOES NOT exists. Do you want to create it [Y/n] : " 
+  read YES_NO
+  [ -z $YES_NO  | $YES_NO = "n"  | $YES_NO = "N" ] && exit
+  mkdir $MOUNT_PATH
+fi
 
 set -e -x -o pipefail
 mount $MOUNT_DEVICE $MOUNT_PATH
