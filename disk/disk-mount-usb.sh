079aba66ba4ea3ee29080468c60938eea6626d25
mysvit <sv0505@gmail.com>
2020-04-18 18:29:44 -0400

    Update disk-mount-usb.sh

--- disk/disk-mount-usb.sh
+++ disk/disk-mount-usb.sh
@@ -4,10 +4,14 @@ clear
 FSTAB="/etc/fstab"
 fdisk -l
 
+echo
 echo "Enter device what you want to mount [/dev/sdc for example] : "
 read MOUNT_DEVICE
+[ -z ${MOUNT_DEVICE} ] && echo "Please provide mount device." && exit
+
 echo "Enter mount path [/mnt/usb for example] : "
 read MOUNT_PATH
+[ -z ${MOUNT_PATH} ] && echo "Please provide mount path." && exit
 
 echo "mount $MOUNT_DEVICE $MOUNT_PATH" >> $FSTAB
 nano $FSTAB
