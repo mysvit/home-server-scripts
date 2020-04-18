4ac2ae3a37807af2e5d2c5674f859f125a6d4b2e
mysvit <sv0505@gmail.com>
2020-04-18 18:48:46 -0400

    Update disk-mount-usb.sh

--- disk/disk-mount-usb.sh
+++ disk/disk-mount-usb.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-# mount usb on startup
 clear
 FSTAB="/etc/fstab"
 fdisk -l
 
 echo
+echo "##########  mount usb and put on startup ##########"
+echo
 echo "Enter device what you want to mount [/dev/sdc for example] : "
 read MOUNT_DEVICE
-[ -z ${MOUNT_DEVICE} ] && echo "Please provide mount device." && exit
+[ -z $MOUNT_DEVICE ] && echo "Please provide mount device." && exit
 
 echo "Enter mount path [/mnt/usb for example] : "
 read MOUNT_PATH
-[ -z ${MOUNT_PATH} ] && echo "Please provide mount path." && exit
+[ -z $MOUNT_PATH ] && echo "Please provide mount path." && exit
+[ ! -d $MOUNT_PATH ] && echo "Directory $MOUNT_PATH DOES NOT exists." && exit
+
+set -e -x -o pipefail
+mount $MOUNT_DEVICE $MOUNT_PATH
 
 echo "mount $MOUNT_DEVICE $MOUNT_PATH" >> $FSTAB
 nano $FSTAB
