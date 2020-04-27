f875eacd2e7edfabe0ad6219d7ac1b92ef85f260
mysvit <sv0505@gmail.com>
2020-04-27 19:19:39 -0400

    Update my-mount.sh

--- my/my-mount.sh
+++ my/my-mount.sh
@@ -1,33 +1,22 @@
 #!/bin/bash
+FSTAB="/etc/fstab"
 
 # HDD 8TB
-f-fstab="/etc/fstab"
-usb-hdd-8T-1="/dev/sdc1"
-mnt-hdd-8T-1="/mnt/hdd-8T-1"
-mount-8T-1 = "mount $usb-hdd-8T-1 $mnt-hdd-8T-1"
+usb-dev-8T-1="/dev/sdc1"
+usb-id-8T-1="FGH09H-GJH984"
+path-8T-1="/mnt/hdd-8T-1"
+echo "# HDD USB 8TB $usb-dev-8T-1" >> $FSTAB
+echo "UUID=$usb-id-8T-1 $path-8T-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
 
 # HDD 500GB
-usb-hdd-500G-1="/dev/sdc2"
-mnt-hdd-500G-1="/mnt/hdd-500G-1"
-mount-500G-1 = "mount $usb-hdd-500G-1 $mnt-hdd-500G-1"
+usb-dev-500G-1="/dev/sdc2"
+usb-id-500G-1="??????-GJH984"
+path-500G-1="/mnt/hdd-500G-1"
+echo "# USB HDD 500GB  $usb-dev-500G-1" >> $FSTAB
+echo "UUID=$usb-dev-500G-1 $path-500G-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
 
-echo
-echo "###################################################################"
-echo "$mounMedia"
-echo "$mounDeluge"
-echo "###################################################################"
-echo
-echo -n "Do you want to add this custome mount to $f-fstab (n : just view) Y/n : "
-read yesNo
+mkdir $path-8T-1
+mkdir $path-500G-1
 
-if [ "$yesNo" = "Y" ] || ["$yesNo" = "y" ]
-then
-  mkdir $mnt-hdd-8T-1
-  mkdir $mnt-hdd-500G-1
-  echo $mount-8T-1 >> $f-fstab
-  echo $mount-500G-1 >> $f-fstab
-  nano $f-fstab
-  mount -a
-else 
-  nano $f-fstab
-fi
+nano $FSTAB
+mount -a
