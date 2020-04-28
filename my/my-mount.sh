d74674154730a823e08d1574f54c59c6a5b24b4f
mysvit <sv0505@gmail.com>
2020-04-27 22:56:13 -0400

    Update my-mount.sh

--- my/my-mount.sh
+++ my/my-mount.sh
@@ -3,14 +3,14 @@ source my-config.sh
 
 # HDD 8TB
 echo "# HDD USB 8TB $USB-DEV-8T-1" >> $FSTAB
-echo "UUID=$USB-ID-8T-1 $PATH-8T-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
+echo "UUID=$USB-ID-8T-1 $MOUNT-PATH-8T-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
 
 # HDD 500GB
 echo "# USB HDD 500GB  $USB-DEV-500G-1" >> $FSTAB
-echo "UUID=$USB-DEV-500G-1 $PATH-500G-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
+echo "UUID=$USB-DEV-500G-1 $MOUNT-PATH-500G-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
 
-mkdir $PATH-8T-1
-mkdir $PATH-500G-1
+mkdir $MOUNT-PATH-8T-1
+mkdir $MOUNT-PATH-500G-1
 
 nano $FSTAB
 mount -a
