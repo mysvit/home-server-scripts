6db799cd172ee3764ef7df1a75ee2f36ef332090
mysvit <sv0505@gmail.com>
2020-04-27 22:35:27 -0400

    Update my-folders.sh

--- my/my-folders.sh
+++ my/my-folders.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
 
 # media, torrent(deluge), backup folders
-d-deluge=$mnt-hdd-500G-1
-sl-deluge="/mnt/deluge"
-d-media="$mnt-hdd-8T-1/media"
-sl-media="/mnt/media"
-d-backup="$mnt-hdd-8T-1/backup"
-sl-backup="/mnt/backup"
-d-backup-jellyfin="$mnt-hdd-8T-1/backup/jellyfin"
+#d-deluge=$mnt-hdd-500G-1
+#sl-deluge="/mnt/deluge"
+#d-media="$mnt-hdd-8T-1/media"
+#sl-media="/mnt/media"
+#d-backup="$mnt-hdd-8T-1/backup"
+#sl-backup="/mnt/backup"
+#d-backup-jellyfin="$mnt-hdd-8T-1/backup/jellyfin"
 
-echo
-echo "###################################################################"
-echo "$mounMedia"
-echo "$mounDeluge"
-echo "###################################################################"
-echo
-echo -n "Do you want to make next folders (n : just view) Y/n : "
-read yesNo
+#mkdir $d-backup-jellyfin
 
-if [ "$yesNo" = "Y" ] || ["$yesNo" = "y" ]
-then
-  mkdir $d-backup-jellyfin
-  ln -s $d-media $sl-media
-  ln -s $d-deluge $sl-deluge
-  ln -s $d-backup $sl-backup
-fi
+#mkdir $J-MEDIA
+
+ln -s $PATH-8T-1/media $SL-J-MEDIA
+#ln -s $d-deluge $sl-deluge
+#ln -s $d-backup $sl-backup
