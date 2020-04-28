73979b86c20c5e0cb7671efb09328bbe2880e58a
mysvit <sv0505@gmail.com>
2020-04-27 22:55:39 -0400

    Update my-config.sh

--- my/my-config.sh
+++ my/my-config.sh
@@ -4,12 +4,12 @@ FSTAB="/etc/fstab"
 # HDD 8TB
 USB-DEV-8T-1="/dev/sdc1"
 USB-ID-8T-1="FGH09H-GJH984"
-PATH-8T-1="/media/hdd-8T-1"
+MOUNT-PATH-8T-1="/media/hdd-8T-1"
 
 # HDD 500GB
 USB-DEV-500G-1="/dev/sdc2"
 USB-ID-500G-1="??????-GJH984"
-PATH-500G-1="/media/hdd-500G-1"
+MOUNT-PATH-500G-1="/media/hdd-500G-1"
 
 # MY ROOT FOLDER
 MY-SVIT="/mysvit"
