50bcad069897e72c075377eab6ccb1f9644d02c1
mysvit <sv0505@gmail.com>
2020-04-27 22:24:55 -0400

    Update my-mount.sh

--- my/my-mount.sh
+++ my/my-mount.sh
@@ -1,22 +1,16 @@
 #!/bin/bash
-FSTAB="/etc/fstab"
+source my-config.sh
 
 # HDD 8TB
-usb-dev-8T-1="/dev/sdc1"
-usb-id-8T-1="FGH09H-GJH984"
-path-8T-1="/mnt/hdd-8T-1"
-echo "# HDD USB 8TB $usb-dev-8T-1" >> $FSTAB
-echo "UUID=$usb-id-8T-1 $path-8T-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
+echo "# HDD USB 8TB $USB-DEV-8T-1" >> $FSTAB
+echo "UUID=$USB-ID-8T-1 $PATH-8T-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
 
 # HDD 500GB
-usb-dev-500G-1="/dev/sdc2"
-usb-id-500G-1="??????-GJH984"
-path-500G-1="/mnt/hdd-500G-1"
-echo "# USB HDD 500GB  $usb-dev-500G-1" >> $FSTAB
-echo "UUID=$usb-dev-500G-1 $path-500G-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
+echo "# USB HDD 500GB  $USB-DEV-500G-1" >> $FSTAB
+echo "UUID=$USB-DEV-500G-1 $PATH-500G-1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
 
-mkdir $path-8T-1
-mkdir $path-500G-1
+mkdir $PATH-8T-1
+mkdir $PATH-500G-1
 
 nano $FSTAB
 mount -a
