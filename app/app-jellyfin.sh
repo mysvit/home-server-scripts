bc234795906b557f0a100fa97fe731f42be3a180
mysvit <sv0505@gmail.com>
2020-05-05 21:20:20 -0400

    Update app-jellyfin.sh

--- app/app-jellyfin.sh
+++ app/app-jellyfin.sh
@@ -7,27 +7,3 @@ wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | apt-key add -
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | tee /etc/apt/sources.list.d/jellyfin.list
 apt update
 apt install jellyfin -y
-
-#restore from backup
-systemctl stop jellyfin
-
-chown -R jellyfin:jellyfin /mnt/backup/jellyfin/*
-find /mnt/backup/jellyfin/ -type d -exec chmod 755 {} \;
-find /mnt/backup/jellyfin/ -type f -exec chmod 644 {} \;
-
-unzip -o /mnt/backup/jellyfin/var-lib-jellyfin.zip -d /
-chown -R jellyfin:jellyfin /var/lib/jellyfin/*
-find /var/lib/jellyfin -type d -exec chmod 755 {} \;
-find /var/lib/jellyfin -type f -exec chmod 644 {} \;
-
-unzip -o /mnt/backup/jellyfin/etc-jellyfin.zip -d /
-chown -R jellyfin:jellyfin /etc/jellyfin/*
-find /etc/jellyfin -type d -exec chmod 755 {} \;
-find /etc/jellyfin -type f -exec chmod 644 {} \;
-
-systemctl start jellyfin
-systemctl status jellyfin
-
-echo
-echo "Press ENTER to continue"
-read
