f5af82a1c6b088e75352c6bef016e5802ecc7067
mysvit <sv0505@gmail.com>
2020-05-10 14:03:37 -0400

    Update my-jellyfin-restore.sh

--- my/my-jellyfin-restore.sh
+++ my/my-jellyfin-restore.sh
@@ -3,16 +3,16 @@ source my-config.sh
 
 systemctl stop jellyfin
 
-chown -R jellyfin:jellyfin $SL-BACKUP/jellyfin/*
-find $SL-BACKUP/jellyfin -type d -exec chmod 755 {} \;
-find $SL-BACKUP/jellyfin -type f -exec chmod 644 {} \;
+chown -R jellyfin:jellyfin "${SL_BACKUP}/jellyfin/*"
+find "${SL_BACKUP}/jellyfin" -type d -exec chmod 755 {} \;
+find "${SL_BACKUP}/jellyfin" -type f -exec chmod 644 {} \;
 
-unzip -o $SL-BACKUP/jellyfin/var-lib-jellyfin.zip -d /
+unzip -o "${SL_BACKUP}/jellyfin/var-lib-jellyfin.zip" -d /
 chown -R jellyfin:jellyfin /var/lib/jellyfin/*
 find /var/lib/jellyfin -type d -exec chmod 755 {} \;
 find /var/lib/jellyfin -type f -exec chmod 644 {} \;
 
-unzip -o $SL-BACKUP/jellyfin/etc-jellyfin.zip -d /
+unzip -o "${SL_BACKUP}/jellyfin/etc-jellyfin.zip" -d /
 chown -R jellyfin:jellyfin /etc/jellyfin/*
 find /etc/jellyfin -type d -exec chmod 755 {} \;
 find /etc/jellyfin -type f -exec chmod 644 {} \;
