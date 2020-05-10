007282d67b0b9aa87d996f8674e8541afe6337c9
mysvit <sv0505@gmail.com>
2020-05-10 13:52:56 -0400

    Update my-jellyfin-backup.sh

--- my/my-jellyfin-backup.sh
+++ my/my-jellyfin-backup.sh
@@ -4,10 +4,10 @@ source my-config.sh
 systemctl stop jellyfin
 
 # data folder
-rm $SL-BACKUP/jellyfin/var-lib-jellyfin.zip
-zip -r1 $SL-BACKUP/jellyfin/var-lib-jellyfin.zip /var/lib/jellyfin/*
+rm "${SL-BACKUP}/jellyfin/var-lib-jellyfin.zip"
+zip -r1 "${SL-BACKUP}/jellyfin/var-lib-jellyfin.zip" /var/lib/jellyfin/*
 # config
-rm $SL-BACKUP/jellyfin/etc-jellyfin.zip
-zip -r1 $SL-BACKUP/jellyfin/etc-jellyfin.zip /etc/jellyfin/*
+rm "${SL-BACKUP}/jellyfin/etc-jellyfin.zip"
+zip -r1 "${SL-BACKUP}/jellyfin/etc-jellyfin.zip" /etc/jellyfin/*
 
 systemctl start jellyfin
