db64cee0ef8f2faad1ed76060a70cc9889e06a3c
mysvit <sv0505@gmail.com>
2020-05-10 16:33:03 -0400

    Update my-deluge-restore.sh

--- my/my-deluge-restore.sh
+++ my/my-deluge-restore.sh
@@ -3,7 +3,7 @@ source my-config.sh
 
 systemctl stop deluge
 
-unzip -o $SL-BACKUP/deluge/config.zip -d /
+unzip -o "${SL_BACKUP}/deluge/config.zip" -d /
 chown -R deluge:deluge /home/deluge/.config/deluge/*
 find /home/deluge/.config/deluge -type d -exec chmod 700 {} \;
 find /home/deluge/.config/deluge -type f -exec chmod 600 {} \;
