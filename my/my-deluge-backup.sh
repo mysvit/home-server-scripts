9ee54041ee00490a0dca52c50104990f55379c5f
mysvit <sv0505@gmail.com>
2020-05-10 16:30:27 -0400

    Update my-deluge-backup.sh

--- my/my-deluge-backup.sh
+++ my/my-deluge-backup.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 source my-config.sh
 
+# make foledr if not exist
+mkdir "${SL_BACKUP}/deluge"
 # backup config
 rm $SL-BACKUP/deluge/config.zip
 zip -r1 $SL-BACKUP/deluge/config.zip /home/deluge/.config/deluge/*
