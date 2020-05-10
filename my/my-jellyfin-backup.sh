f7a8dc8a11a8b74c2e49d3553f64982bfa291d7d
mysvit <sv0505@gmail.com>
2020-05-10 16:24:48 -0400

    Update my-jellyfin-backup.sh

--- my/my-jellyfin-backup.sh
+++ my/my-jellyfin-backup.sh
@@ -3,6 +3,8 @@ source my-config.sh
 
 systemctl stop jellyfin
 
+# make foledr if not exist
+mkdir "${SL_BACKUP}/jellyfin"
 # data folder
 rm "${SL_BACKUP}/jellyfin/var-lib-jellyfin.zip"
 zip -r1 "${SL_BACKUP}/jellyfin/var-lib-jellyfin.zip" /var/lib/jellyfin/*
