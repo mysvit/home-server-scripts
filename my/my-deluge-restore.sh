ac5188a1b9c47255bb301005e4fd242a1c6aefa7
mysvit <sv0505@gmail.com>
2020-05-19 23:19:14 -0400

    Update my-deluge-restore.sh

--- my/my-deluge-restore.sh
+++ my/my-deluge-restore.sh
@@ -3,7 +3,7 @@ source my-config.sh
 
 systemctl stop deluge
 
-unzip -o "${SL_BACKUP}/deluge/config.zip" -d /
+unzip -o "${MYSVIT_BACKUP}/deluge/config.zip" -d /
 chown -R deluge:deluge /home/deluge/.config/deluge/*
 find /home/deluge/.config/deluge -type d -exec chmod 700 {} \;
 find /home/deluge/.config/deluge -type f -exec chmod 600 {} \;
