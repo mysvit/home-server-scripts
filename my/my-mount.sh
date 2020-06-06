e510a65fc63909fdf9dc921074d522e3a13085b0
mysvit <sv0505@gmail.com>
2020-06-06 16:51:31 -0400

    Update my-mount.sh

--- my/my-mount.sh
+++ my/my-mount.sh
@@ -6,11 +6,11 @@ echo "# HDD USB 8TB $USB_DEV_8T_1" >> $FSTAB
 echo "UUID=$USB_ID_8T_1 $MOUNT_PATH_8T_1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
 
 # HDD 500GB
-echo "# USB HDD 500GB  $USB_DEV_500G_1" >> $FSTAB
-echo "UUID=$USB_DEV_500G_1 $MOUNT_PATH_500G_1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
+#echo "# USB HDD 500GB  $USB_DEV_500G_1" >> $FSTAB
+#echo "UUID=$USB_DEV_500G_1 $MOUNT_PATH_500G_1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
 
 mkdir $MOUNT_PATH_8T_1
-mkdir $MOUNT_PATH_500G_1
+#mkdir $MOUNT_PATH_500G_1
 
 nano $FSTAB
 mount -a
