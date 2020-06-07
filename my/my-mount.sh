0c806bcc2ef966adbed86df7123f8f461b50f006
mysvit <sv0505@gmail.com>
2020-06-06 23:12:27 -0400

    Update my-mount.sh

--- my/my-mount.sh
+++ my/my-mount.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 source ./my/my-config.sh
+apt install -y ntfs-3g
 
 # HDD 8TB
 echo "# HDD USB 8TB $USB_DEV_8T_1" >> $FSTAB
-echo "UUID=$USB_ID_8T_1 $MOUNT_PATH_8T_1 auto nosuid,nodev,nofail 0 0" >> $FSTAB
+echo "UUID=$USB_ID_8T_1 $MOUNT_PATH_8T_1   ntfs-3g        defaults    0    0" >> $FSTAB
+
 
 # HDD 500GB
 #echo "# USB HDD 500GB  $USB_DEV_500G_1" >> $FSTAB
