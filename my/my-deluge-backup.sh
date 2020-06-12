a7359b1d3d3524497ded06310925cbcb68a55373
mysvit <sv0505@gmail.com>
2020-06-12 16:09:05 -0400

    Update my-deluge-backup.sh

--- my/my-deluge-backup.sh
+++ my/my-deluge-backup.sh
@@ -2,10 +2,15 @@
 source /home/home-server-scripts/my/my-config.sh
 
 # make foledr if not exist
-mkdir "${MYSVIT_BACKUP}/deluge"
+if [ ! -d "${MYSVIT_BACKUP}/deluge" ]
+then
+  mkdir "${MYSVIT_BACKUP}/deluge"
+fi
+
 # backup config
 if [ -f "${MYSVIT_BACKUP}/deluge/config.zip" ]
 then
   rm "${MYSVIT_BACKUP}/deluge/config.zip"
 fi
+
 zip -r1 "$MYSVIT_BACKUP/deluge/config.zip" /home/deluge/.config/deluge/*
