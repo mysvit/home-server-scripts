22ada77defe69f46fdf6fb660f39ad28b225fed2
mysvit <sv0505@gmail.com>
2020-06-12 16:33:32 -0400

    Update app-deluge-uninstall.sh

--- app/app-deluge-uninstall.sh
+++ app/app-deluge-uninstall.sh
@@ -2,15 +2,12 @@
 
 systemctl stop deluged
 systemctl disable deluged
-deluged_service="/etc/systemd/system/deluged.service"
+apt remove deluged -y
+rm "/etc/systemd/system/deluged.service"
 
 systemctl stop deluge-web
 systemctl disable deluge-web
-deluge_web_service="/etc/systemd/system/deluge-web.service"
-
-apt remove deluged -y
 apt remove deluge-webui -y
-deluser deluge
+rm "/etc/systemd/system/deluge-web.service"
 
-rm $deluged_service
-rm $deluge_web_service
+deluser deluge
