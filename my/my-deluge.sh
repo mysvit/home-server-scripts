7dfc030acdadfd25e6297dd7ca384bb7634e2816
mysvit <sv0505@gmail.com>
2020-06-12 21:57:27 -0400

    Update my-deluge.sh

--- my/my-deluge.sh
+++ my/my-deluge.sh
@@ -2,6 +2,7 @@
 
 source ./my/my-config.sh
 
+# configure backup
 chown -R deluge:deluge "${MYSVIT_BACKUP}/deluge/*"
 find "${MYSVIT_BACKUP}/deluge" -type d -exec chmod 755 {} \;
 find "${MYSVIT_BACKUP}/deluge" -type f -exec chmod 655 {} \;
@@ -9,6 +10,7 @@ find "${MYSVIT_BACKUP}/deluge" -type f -exec chmod 655 {} \;
 # add backup config to crontab
 echo "0 1 * * * /bin/sh /home/home-server-scripts/my/my-deluge-backup.sh" > "${CRONTAB_DIR}\deluge"
 
+# install deluge
 bash ./app/app-deluge.sh
 
 echo
@@ -17,4 +19,5 @@ echo ***************************************************************************
 echo
 echo
 
+# restore from backup
 bash ./my/my-deluge-restore.sh
