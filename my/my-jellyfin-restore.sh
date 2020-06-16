da03b81d35e89826c607b6df923c46662f6b6e8b
mysvit <sv0505@gmail.com>
2020-06-15 22:51:53 -0400

    Update my-jellyfin-restore.sh

--- my/my-jellyfin-restore.sh
+++ my/my-jellyfin-restore.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
-source ./my/my-config.sh
+source /home/home-server-scripts/my/my-config.sh
 
-systemctl stop jellyfin
+if [ -f "${MYSVIT_BACKUP}/jellyfin/var-lib-jellyfin.zip" ] && [ -f "${MYSVIT_BACKUP}/jellyfin/etc-jellyfin.zip" ]
+then
+  systemctl stop jellyfin
 
-chown -R jellyfin:jellyfin "${MYSVIT_BACKUP}/jellyfin/*"
-find "${MYSVIT_BACKUP}/jellyfin" -type d -exec chmod 755 {} \;
-find "${MYSVIT_BACKUP}/jellyfin" -type f -exec chmod 644 {} \;
+  chown -R jellyfin:jellyfin "${MYSVIT_BACKUP}/jellyfin/*"
+  find "${MYSVIT_BACKUP}/jellyfin" -type d -exec chmod 755 {} \;
+  find "${MYSVIT_BACKUP}/jellyfin" -type f -exec chmod 644 {} \;
 
-unzip -o "${MYSVIT_BACKUP}/jellyfin/var-lib-jellyfin.zip" -d /
-chown -R jellyfin:jellyfin /var/lib/jellyfin/*
-find /var/lib/jellyfin -type d -exec chmod 755 {} \;
-find /var/lib/jellyfin -type f -exec chmod 644 {} \;
+  unzip -o "${MYSVIT_BACKUP}/jellyfin/var-lib-jellyfin.zip" -d /
+  chown -R jellyfin:jellyfin /var/lib/jellyfin/*
+  find /var/lib/jellyfin -type d -exec chmod 755 {} \;
+  find /var/lib/jellyfin -type f -exec chmod 644 {} \;
 
-unzip -o "${MYSVIT_BACKUP}/jellyfin/etc-jellyfin.zip" -d /
-chown -R jellyfin:jellyfin /etc/jellyfin/*
-find /etc/jellyfin -type d -exec chmod 755 {} \;
-find /etc/jellyfin -type f -exec chmod 644 {} \;
+  unzip -o "${MYSVIT_BACKUP}/jellyfin/etc-jellyfin.zip" -d /
+  chown -R jellyfin:jellyfin /etc/jellyfin/*
+  find /etc/jellyfin -type d -exec chmod 755 {} \;
+  find /etc/jellyfin -type f -exec chmod 644 {} \;
 
-systemctl start jellyfin
-systemctl status jellyfin
+  systemctl start jellyfin -l --no-pager
+  systemctl status jellyfin -l --no-pager
+fi
