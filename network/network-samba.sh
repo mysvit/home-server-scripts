7818c941e9b448a866c0d9dfbfea4739065b90ec
mysvit <sv0505@gmail.com>
2020-06-19 13:13:46 -0400

    Create network-samba.sh

---
+++ network/network-samba.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cho
+echo -n "Enter path for share : "
+read sharePath
+echo -n "Enter user name for share : "
+read shareUser
+smbpasswd -a $shareUser
+
+if [! -z "$sharePath"] && [! -z $shareUser]
+then
+
+echo "
+[mysvit]
+   comment = My Svit Share
+   path = $sharePath
+   browseable = yes
+   read only = no
+   guest ok = no
+   valid users = $shareUser
+" >> /etc/samba/smb.conf
+
+  nano /etc/samba/smb.conf
+
+else
+
+  nano /etc/samba/smb.conf
+  
+fi
