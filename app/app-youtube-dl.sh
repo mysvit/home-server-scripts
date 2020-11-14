ca8db184c84d55cfcc500aa308f36860ee3c929c
stmichaelmontreal <stmichaelmontreal@gmail.com>
2020-11-14 10:59:01 -0500

    youtube-dl /usr/bin

--- app/app-youtube-dl.sh
+++ app/app-youtube-dl.sh
@@ -5,5 +5,5 @@ echo Youtube-dl
 echo 
 apt install youtube-dl -y
 curl -L https://yt-dl.org/downloads/latest/youtube-dl -o /usr/local/bin/youtube-dl
-chmod a+rx /usr/local/bin/youtube-dl
+chmod a+rx /usr/bin/youtube-dl
 
