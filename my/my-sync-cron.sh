e818279a2fb8bf74c1f55bc95eb8ffdd7e4fd13c
stmichaelmontreal <stmichaelmontreal@gmail.com>
2020-11-28 18:20:35 -0500

    add sync to menu

--- my/my-sync-cron.sh
+++ my/my-sync-cron.sh
@@ -7,6 +7,10 @@ echo ""
 echo "***********************************  sync  **********************************************"
 echo ""
 
-mkdir /var/log/mysvit-sync
+if [ ! -d "/var/log/mysvit-sync" ]
+then
+  mkdir /var/log/mysvit-sync
+fi
+
 # add sync to crontab
 echo "0 3 * * * /bin/sh /home/home-server-scripts/my/my-sync.sh" >> "${CRONTAB_ROOT}"
