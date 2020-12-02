7da669d2d40f0fc8093fc5c7fab5bfeec5df39f1
stmichaelmontreal <stmichaelmontreal@gmail.com>
2020-12-02 13:06:31 -0500

    rsync log

--- my/my-sync-with-delete.sh
+++ my/my-sync-with-delete.sh
@@ -11,8 +11,11 @@ echo ""
 rsync -arvn --delete-before /media/hdd-8T-1/ /media/hdd-8T-3/
 
 echo "Do you want run this command for sync with delete: rsync -arv --delete-before /media/hdd-8T-1/ /media/hdd-8T-3/"
-echo "[Y/N]:"
+echo -n "[Y/N]:"
 read YES_NO
 [ -z $YES_NO ] || [ "$YES_NO" = "n" ] || [ "$YES_NO" = "N" ] && echo "Answer no! Script canceled!" && exit
 
+dt=$(date '+%d/%m/%Y %H:%M:%S');
+echo "$dt" >> /var/log/mysvit-sync/sync.log
+echo "rsync -arv --delete-before /media/hdd-8T-1/ /media/hdd-8T-3/"
 rsync -arv --delete-before /media/hdd-8T-1/ /media/hdd-8T-3/
--- my/my-sync.sh
+++ my/my-sync.sh
@@ -9,4 +9,5 @@ echo ""
 
 dt=$(date '+%d/%m/%Y %H:%M:%S');
 echo "$dt" >> /var/log/mysvit-sync/sync.log
+echo "rsync -arv /media/hdd-8T-1/ /media/hdd-8T-3/ >> /var/log/mysvit-sync/sync.log"
 rsync -arv /media/hdd-8T-1/ /media/hdd-8T-3/ >> /var/log/mysvit-sync/sync.log
