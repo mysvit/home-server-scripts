25fe7067b03793730216f9543289c8fc30510b88
stmichaelmontreal <stmichaelmontreal@gmail.com>
2020-12-02 13:05:03 -0500

    rsync with delete 2

--- my/my-sync-with-delete.sh
+++ my/my-sync-with-delete.sh
@@ -5,12 +5,13 @@ source ./my/my-config.sh
 
 echo ""
 echo "***********************************  sync  **********************************************"
+echo "rsync -arvn --delete-before /media/hdd-8T-1/ /media/hdd-8T-3/"
 echo ""
 
 rsync -arvn --delete-before /media/hdd-8T-1/ /media/hdd-8T-3/
 
-echo -n "Do you want run this command for sync with delete: rsync -arv --delete-before /media/hdd-8T-1/ /media/hdd-8T-3/"
-echo -n "[Y/N]:"
+echo "Do you want run this command for sync with delete: rsync -arv --delete-before /media/hdd-8T-1/ /media/hdd-8T-3/"
+echo "[Y/N]:"
 read YES_NO
 [ -z $YES_NO ] || [ "$YES_NO" = "n" ] || [ "$YES_NO" = "N" ] && echo "Answer no! Script canceled!" && exit
 
