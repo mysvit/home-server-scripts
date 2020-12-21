ba1743822d526d71d98c8ca42645edcfe4eba172
stmichaelmontreal <stmichaelmontreal@gmail.com>
2020-12-21 15:42:31 -0500

    permission

--- my/my-sync.sh
+++ my/my-sync.sh
@@ -13,8 +13,8 @@ echo "rsync -arv /media/hdd-8T-1/ /media/hdd-8T-3/ >> ${MYSVIT_LOG}/sync.log" >>
 
 chown -R jellyfin:jellyfin /media/hdd-8T-1/
 chown -R jellyfin:jellyfin /media/hdd-8T-3/
-chmod -R 755 /media/hdd-8T-1/
-chmod -R 755 /media/hdd-8T-3/
+find /media/hdd-8T-1 \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
+find /media/hdd-8T-3 \( -type d -exec chmod 755 {} \; \) -o \( -type f -exec chmod 644 {} \; \)
 
 #rsync -arv --delete-before /media/hdd-8T-1/ /media/hdd-8T-3/ >> "${MYSVIT_LOG}/sync.log"
 rsync -arv /media/hdd-8T-1/ /media/hdd-8T-3/ >> "${MYSVIT_LOG}/sync.log"
