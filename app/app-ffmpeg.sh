05090f6660ea46346bc4cc3484d45b3aa42864a8
stmichaelmontreal <stmichaelmontreal@gmail.com>
2020-12-26 18:11:40 -0500

    mkvtoolnix

--- app/app-ffmpeg.sh
+++ app/app-ffmpeg.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+wget -q -O - https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt | apt-key add -
+echo "deb https://mkvtoolnix.download/debian/ buster main" > /etc/apt/sources.list.d/mkvtoolnix.download.list
 apt update
 echo 
 echo FFMPEG & MKVTOOLNIX
