fe76801f1e4c080fa51535e449c189f3205f17a2
stmichaelmontreal <stmichaelmontreal@gmail.com>
2021-08-17 15:10:45 -0400

    restore to ssd

--- my/my-restore-all.sh
+++ my/my-restore-all.sh
@@ -5,11 +5,12 @@ source /home/home-server-scripts/my/my-config.sh
 bash /home/home-server-scripts/my-mount.sh
 # create directory structure
 bash /home/home-server-scripts/my-folders.sh
-
+# restore data from ssd
+bash /home/home-server-scripts/my-sync-restore.sh
+# restore app
 bash /home/home-server-scripts/my-duckdns-restore.sh
 echo '0 1 * * * bash -c "chmod -R 744 /home/home-server-scripts"' >> "${CRONTAB_ROOT}"
 bash /home/home-server-scripts/my-caddy-restore.sh
 bash /home/home-server-scripts/my-deluge-restore.sh
 bash /home/home-server-scripts/my-jellyfin-restore.sh
 bash /home/home-server-scripts/my-samba-restore.sh
-bash /home/home-server-scripts/my-sync-restore.sh
