b26eda3eb9f6c267e7b2278584683a535504338e
stmichaelmontreal <stmichaelmontreal@gmail.com>
2021-08-17 22:50:37 -0400

    app jellyfin fix

--- app/app-jellyfin.sh
+++ app/app-jellyfin.sh
@@ -2,7 +2,7 @@
 
 # Install jellyfin
 apt update
-apt install -y wget apt-transport-https ca-certificates curl gnupg2 software-properties-common
+apt install -y wget apt-transport-https ca-certificates curl gnupg2
 wget -O - https://repo.jellyfin.org/jellyfin_team.gpg.key | apt-key add -
 echo "deb [arch=$( dpkg --print-architecture )] https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | tee /etc/apt/sources.list.d/jellyfin.list
 apt update
