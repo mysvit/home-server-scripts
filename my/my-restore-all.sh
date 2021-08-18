5b2dfb4344cd5e6593571f2dafd2fa1259881665
stmichaelmontreal <stmichaelmontreal@gmail.com>
2021-08-17 20:06:15 -0400

    fix restore my

--- my/my-restore-all.sh
+++ my/my-restore-all.sh
@@ -2,15 +2,15 @@
 source /home/home-server-scripts/my/my-config.sh
 
 # create map directory and mount hdd
-bash /home/home-server-scripts/my-mount.sh
+bash /home/home-server-scripts/my/my-mount.sh
 # create directory structure
-bash /home/home-server-scripts/my-folders.sh
+bash /home/home-server-scripts/my/my-folders.sh
 # restore data from ssd
-bash /home/home-server-scripts/my-sync-restore.sh
+bash /home/home-server-scripts/my/my-sync-restore.sh
 # restore app
-bash /home/home-server-scripts/my-duckdns-restore.sh
+bash /home/home-server-scripts/my/my-duckdns-restore.sh
 echo '0 1 * * * bash -c "chmod -R 744 /home/home-server-scripts"' >> "${CRONTAB_ROOT}"
-bash /home/home-server-scripts/my-caddy-restore.sh
-bash /home/home-server-scripts/my-deluge-restore.sh
-bash /home/home-server-scripts/my-jellyfin-restore.sh
-bash /home/home-server-scripts/my-samba-restore.sh
+bash /home/home-server-scripts/my/my-caddy-restore.sh
+bash /home/home-server-scripts/my/my-deluge-restore.sh
+bash /home/home-server-scripts/my/my-jellyfin-restore.sh
+bash /home/home-server-scripts/my/my-samba-restore.sh
