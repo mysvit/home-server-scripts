02a035cad4301dff32a2eb54b840b44b0e4bde2e
stmichaelmontreal <stmichaelmontreal@gmail.com>
2021-08-19 09:43:28 -0400

    tune wifi

--- my/my-system-tune.sh
+++ my/my-system-tune.sh
@@ -4,7 +4,7 @@ source /home/home-server-scripts/my/my-config.sh
 echo ""
 echo "***********************************   disable bluetooth ***************************************"
 
-"# disable bluetooth module to check run #lsmod
+echo "# disable bluetooth module to check run #lsmod
 blacklist btusb
 blacklist btrtl
 blacklist btbcm
@@ -12,9 +12,17 @@ blacklist btintel
 blacklist bluetooth
 " | tee /etc/modprobe.d/bluetooth.conf
 
-echo ""
 echo "***********************************   disable bluetooth ***************************************"
 echo ""
+echo "***********************************   disable wifi      ***************************************"
+
+echo "# disable wifi module to check run #lsmod
+blacklist iwlwifi
+blacklist cfg80211
+" | tee /etc/modprobe.d/wifi.conf
+
+echo ""
+echo "***********************************   disable wifi      ***************************************"
 
 echo "Press Enter to continue..."
 read
