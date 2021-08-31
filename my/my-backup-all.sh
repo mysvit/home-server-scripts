8e371a8c00c200d8830749a654ba907ab84bb90a
stmichaelmontreal <stmichaelmontreal@gmail.com>
2021-08-31 08:52:57 -0400

    backup all

--- my/my-backup-all.sh
+++ my/my-backup-all.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 bash /home/home-server-scripts/my-caddy-backup.sh
 bash /home/home-server-scripts/my-deluge-backup.sh
+bash /home/home-server-scripts/my-jellyfin-backup.sh
+bash /home/home-server-scripts/my-samba-backup.sh
+bash /home/home-server-scripts/my-sync.sh
