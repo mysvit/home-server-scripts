20ff3773157e361c0bd7386656260127e3109670
stmichaelmontreal <stmichaelmontreal@gmail.com>
2021-08-31 12:45:18 -0400

    mkvtools

--- app/app-ffmpeg.sh
+++ app/app-ffmpeg.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-wget -q -O - https://mkvtoolnix.download/gpg-pub-moritzbunkus.txt | apt-key add -
-echo "deb [signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release ) $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release ) main" | tee /etc/apt/sources.list.d/mkvtoolnix.download.list
+wget -O /usr/share/keyrings/gpg-pub-moritzbunkus.gpg https://mkvtoolnix.download/gpg-pub-moritzbunkus.gpg
+echo "deb [signed-by=/usr/share/keyrings/gpg-pub-moritzbunkus.gpg] https://mkvtoolnix.download/debian/ bullseye main" > /etc/apt/sources.list.d/mkvtoolnix.download.list
 apt update
 echo "******************************************************************************"
 echo "FFMPEG & MKVTOOLNIX"
