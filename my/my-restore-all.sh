14b31ff0e9ae1a66328d02a89fb00bb4b77a7fc5
stmichaelmontreal <stmichaelmontreal@gmail.com>
2021-08-31 11:23:12 -0400

    sftp setup

--- my/my-restore-all.sh
+++ my/my-restore-all.sh
@@ -17,5 +17,5 @@ bash /home/home-server-scripts/my/my-caddy-restore.sh
 bash /home/home-server-scripts/my/my-deluge-restore.sh
 bash /home/home-server-scripts/my/my-jellyfin-restore.sh
 bash /home/home-server-scripts/my/my-samba-restore.sh
-
+bash /home/home-server-scripts/my/my-sftp.sh
 service cron reload
--- my/my-sftp.sh
+++ my/my-sftp.sh
@@ -4,21 +4,25 @@ source /home/home-server-scripts/my/my-config.sh
 # install sftp
 
 echo ""
-echo "***********************************  sftp  **********************************************"
+echo "***********************************  SFTP setup **********************************************"
 echo ""
 
 # create group
 groupadd sftp_users
 # create user
-useradd -m -G sftp_users sftp_user
-echo "sftp_user:myPass" | chpasswd
+echo -n "Enter sftp user : "
+read SFTP_USER
+echo -n "Enter sftp password : "
+read SFTP_PWD
+useradd -m -G sftp_users $SFTP_USER
+echo "$SFTP_USER:$SFTP_PWD" | chpasswd
 
-cat << EOF >> /etc/ssh/sshd_config.d/sftp_access.conf
+echo "
 Match Group sftp_users
 ChrootDirectory /sftp_folder
 X11Forwarding no
 AllowTcpForwarding no
 ForceCommand internal-sftp
-EOF
+" > /etc/ssh/sshd_config.d/sftp_access.conf
 
 systemctl restart sshd
