b769c09e3c31ab96dc902a997fa71773e610c3ec
Sergii Lutchyn <sergii@empower.social>
2022-06-16 10:47:00 +0300

    xsessionrc

---
+++ my/1_displays.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+xrandr --setprovideroutputsource modesetting NVIDIA-0
+xrandr --output DP-0 --off --output DP-1 --off --output HDMI-0 --off --output eDP-1-1 --primary --mode 1920x1080 --pos 0x0 --rotate normal
+
